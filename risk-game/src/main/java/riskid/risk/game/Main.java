a5e93118873847b08722803a4f403cd7016a1d93
Isosaari Matias L J <imatias@da5-cs-bk107-02.helsinki.fi>
2016-03-15 16:31:06 +0200

    maven

---
+++ risk-game/src/main/java/riskid/risk/game/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riskid.risk.game;
+
+/**
+ *
+ * @author imatias
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
